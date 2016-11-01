60d364e357d9c5b3e3fad6a7216bee06e6bc4015
Tyler Moon <moon.tyler@gmail.com>
2016-11-01 17:21:24 -0400

    Tested out new message architecture using RabbitMQ

---
+++ rabbitmq/systemTest/rabbitmqHandler.py
@@ -0,0 +1,17 @@
+import pika
+
+class rabbitMQHandler(object):
+    def __init__(self, exchange, routing_key, queueType):
+        self.exchange = exchange
+        self.routing_key = routing_key
+        self.queueType = queueType
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+                host='localhost'))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=exchange, type=queueType)
+        print("  [q] Declared an exchanged %r of type %r" % (self.exchange,self.queueType))
+    def publishMsg(self, msg):
+        self.channel.basic_publish(self.exchange, self.routing_key, msg)
+        print("  [q] (rabbitMQHandler) Send: %r" % msg)
+    def closeConnection(self):
+        self.connection.close()
