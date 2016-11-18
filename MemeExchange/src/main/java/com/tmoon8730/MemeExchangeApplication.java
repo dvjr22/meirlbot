083f5ffaf61a10bfe51136c1b19467543c6250f0
Tyler Moon <moon.tyler@gmail.com>
2016-11-17 20:38:42 -0500

    Start of the MemeExchange

---
+++ MemeExchange/src/main/java/com/tmoon8730/MemeExchangeApplication.java
@@ -0,0 +1,12 @@
+package com.tmoon8730;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemeExchangeApplication.class, args);
+	}
+}
---
+++ MemeExchange/src/test/java/com/tmoon8730/MemeExchangeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tmoon8730;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemeExchangeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
