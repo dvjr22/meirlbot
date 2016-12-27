86a9a63864145fcd258546e3dd47cc1147aee4bb
alcamech <alcamech@gmail.com>
2016-12-27 14:04:24 -0500

    Added unit test for the MemeAPI

--- MemeAPI/src/test/java/com/tmoon8730/MemeApiApplicationTests.java
+++ MemeAPI/src/test/java/com/tmoon8730/MemeApiApplicationTests.java
@@ -25,7 +25,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
@@ -66,14 +68,15 @@ public class MemeApiApplicationTests {
 		doReturn(returnPost).when(service).findRedditPostById("test123");
 		
 		// Mock the return of the test variable to see if the new object is passed
-		when(returnPost.isMemeFlag()).thenReturn(true);
+		//when(returnPost.isMemeFlag()).thenReturn(true);
+		when(returnPost.getId()).thenReturn("test123");
 		
 		// Call the controller
 		RedditPost testResult = controller.findById("test123");
 		
 		// Assertions to see if it worked
 		assertNotNull(testResult);
-		assertEquals(true,testResult.isMemeFlag());
+		assertEquals("test123",testResult.getId());
 		
 		// Verify that the method was called
 		verify(service, atLeastOnce()).findRedditPostById("test123");
@@ -106,6 +109,86 @@ public class MemeApiApplicationTests {
 	
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	//Null Pointer Exception @ MongoDBRedditPostService.java:110
+	public void testFindByRedditId() throws Exception{
+		// Mock a RedditPost object to be returned by the repository
+		RedditPost post = mock(RedditPost.class);
+		
+		// Mock method returns to avoid calling the database
+		doReturn(post).when(service).findByRedditId("test123");
+		
+		// Mock the return of the test variable to see if the new object is passed
+		when(post.getRedditId()).thenReturn("test123");
+				
+		// Call the controller
+		RedditPost testResults = controller.findByRedditId("test123");
+		
+		// Assertions to see if it worked
+		assertNotNull(testResults);
+		assertEquals("test123",testResults.getRedditId());
+		
+		// Verify  that the method was called
+		verify(service, atLeastOnce()).findByRedditId("test123");
+	}
+	
+	@Test
+	public void testDelete() throws Exception{
+		// Mock a RedditPost object to be returned by the repository
+		RedditPost post = mock(RedditPost.class);
+			
+		// Mock method returns to avoid calling the database
+		doReturn(post).when(service).delete("test123");
+		
+		// Call the controller
+		RedditPost testResults = controller.delete("test123");
+		
+		// Assertions to see if it worked 
+		assertNotNull(testResults);
+		assertEquals(null,testResults.getId());
+		
+		// Verify that the method was called
+		verify(service, atLeastOnce()).delete("test123");	
+	}
+	
+	@Test
+	public void testCreate() throws Exception{
+		// Mock a RedditPost object to be returned by the repository
+		RedditPost post = mock(RedditPost.class);
+		
+		// Mock method returns to avoid calling the database
+		doReturn(post).when(service).create(post);
+		
+		// Call the controller
+		ResponseEntity<RedditPost> testResults = controller.create("test123", post);
+		
+		// Assertions to see if it workedS
+		assertNotNull(testResults);
+		assertEquals(HttpStatus.OK,testResults.getStatusCode());
+		
+		// Verify that the method was called
+		verify(service, atLeastOnce()).create(post);
+	}
+	
+	@Test
+	public void testUpdate() throws Exception{
+		// Mock a RedditPost object to be returned by the repository
+		RedditPost post = mock(RedditPost.class);
+		
+		// Mock method returns to avoid calling the database
+		doReturn(post).when(service).update(post);
+		
+		// Call the controller
+		ResponseEntity<RedditPost> testResults = controller.update("test123", post);
+		
+		// Assertions to see if it worked
+		assertNotNull(testResults);
+		assertEquals(HttpStatus.OK,testResults.getStatusCode());
+		
+		// Verify that the method was called
+		verify(service, atLeastOnce()).update(post);
 	}
 
 }
